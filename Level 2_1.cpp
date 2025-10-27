a0511f720acd142b4a565d7bab1dda68f6692026
Murad-glitch <muradduniyev6@gmail.com>
2025-10-27 22:14:24 +0400

    Add C++ program to compare two integers

---
+++ Level 2_1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    if (a>b) {
+        cout<<a;
+    }
+    if (b>a) {
+        cout<<b;
+    }
+    if (a==b) {
+        cout<<"Bərabərdilər: ";
+    }
+    return 0;
+}
