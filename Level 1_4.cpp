a6760442c51908edd0b0b1fe0d007d27ee46da6c
Murad-glitch <muradduniyev6@gmail.com>
2025-10-27 22:03:42 +0400

    Implement circle area and circumference calculation
    Calculate area and circumference of a circle based on user input.

---
+++ Level 1_4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <cmath>
+using namespace std;
+int main()
+{
+    float c;
+    cin>>c;
+    cout<<M_PI*pow(c,2)<<" "<<2*M_PI*c;
+    return 0;
+}
