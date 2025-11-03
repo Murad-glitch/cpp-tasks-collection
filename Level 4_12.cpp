960c1964fdb140bb250f3050c205f10eaf0d5012
Murad-glitch <muradduniyev6@gmail.com>
2025-11-03 22:08:13 +0400

    Create Level 4_12.cpp

---
+++ Level 4_12.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    b=0;
+    cin>>a;
+    for(c=1;c<a;c++){
+        if(a%c==0){
+            b=b+c;
+        }
+    }
+    if(b==a){
+        cout<<"Perfect";
+    }
+    else{
+        cout<<"Not";
+    }
+    return 0;
+}
