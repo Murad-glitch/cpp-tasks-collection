45e69c2f4f8c28b29df904828cedb5c37cffe3df
Murad-glitch <muradduniyev6@gmail.com>
2025-11-03 20:07:11 +0400

    Change variable name from sum to a for clarity

--- Level 4_7.cpp
+++ Level 4_7.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 
 int main(){
-    int n, m, q, sum=0;
+    int n, m, q, a=0;
     cout<<"Ədədi daxil edin: ";
     cin>>n;
     m=n;
 
     while(m>0){
         q=m%10;
-        sum=sum+q*q;
+        a=a+q*q;
         m=m/10;
     }
 
-    if(sum==n)
+    if(a==n)
         cout<<n<<" rəqəmlərinin kvadratları cəminə bərabərdir."<<endl;
     else
         cout<<n<<" rəqəmlərinin kvadratları cəminə bərabər deyil."<<endl;
